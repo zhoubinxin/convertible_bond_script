cdf931cbf1389dd64570063f25e8a1f052d7460f
zhoubinxin <binxin.zhou@outlook.com>
2024-01-12 16:16:06 +0800

    支持保存债券余额数据/债券评级

--- iFind5.py
+++ iFind5.py
@@ -16,7 +16,7 @@ class FileHandler:
 
     # 保存数据到Excel
     def save_to_excel(self, file_name, str_date, premium):
-        print(f'{str_date}数据保存中')
+        print(f'{str_date} 数据保存中...')
         while True:
             try:
                 if not os.path.exists(file_name):
@@ -53,49 +53,62 @@ class Ths:
 
     # 获取债券余额数据和债券评级
     def get_bond(self, jydm, ths_date, consider_balance, consider_issue):
-        jydm = ', '.join(jydm)
-        print(f'{ths_date} 债券余额数据和债券评级')
         data_balances = []
         data_issues = []
 
-        # ths_bond_balance_cbond债券余额数据 ths_issue_credit_rating_cbond债券评级
-        if consider_balance and consider_issue:
-            data = THS_DS(jydm, 'ths_bond_balance_cbond;ths_issue_credit_rating_cbond', ';', '', ths_date, ths_date,
-                          'format:list')
-        elif consider_balance:
-            data = THS_DS(jydm, 'ths_bond_balance_cbond', ',', '', ths_date, ths_date, 'format:list')
-        elif consider_issue:
-            data = THS_DS(jydm, 'ths_issue_credit_rating_cbond', ',', '', ths_date, ths_date, 'format:list')
-        else:
-            return None, None
+        filename = ths_date[:4] + ths_date[5:7] + ths_date[8:]
+        json_file_path = f'data/{filename}.json'
 
-        if data.data is None:
-            print(data.errmsg)
-        elif consider_balance:
-            for item in data.data:
+        # 从文件读取 JSON 数据
+        with open(json_file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if "ths_bond_balance_cbond" not in data[0]:
+            print(f'{ths_date} 债券余额数据和债券评级')
+            jydm = ', '.join(jydm)
+
+            # ths_bond_balance_cbond债券余额数据 ths_issue_credit_rating_cbond债券评级
+            bond_data = THS_DS(jydm, 'ths_bond_balance_cbond;ths_issue_credit_rating_cbond', ';', '', ths_date,
+                               ths_date, 'format:list')
+
+            if bond_data.data is None:
+                print(data.errmsg)
+
+            for item in bond_data.data:
                 table = item.get('table', {})
                 ths_bond_balance_cbond = table.get('ths_bond_balance_cbond', [])
+                ths_issue_credit_rating_cbond = table.get('ths_issue_credit_rating_cbond', [])
 
                 if isinstance(ths_bond_balance_cbond, list) and ths_bond_balance_cbond:
                     data_balances.append(ths_bond_balance_cbond[0])
                 else:
                     data_balances.append(None)
-        elif consider_issue:
-            for item in data.data:
-                table = item.get('table', {})
-                ths_issue_credit_rating_cbond = table.get('ths_issue_credit_rating_cbond', [])
 
                 if isinstance(ths_issue_credit_rating_cbond, list) and ths_issue_credit_rating_cbond:
                     data_issues.append(ths_issue_credit_rating_cbond[0])
                 else:
                     data_issues.append(None)
 
+            data[0]["ths_bond_balance_cbond"] = data_balances
+            data[0]["ths_issue_credit_rating_cbond"] = data_issues
+
+            # 将修改后的数据写回文件
+            with open(json_file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+        else:
+            print(f'{ths_date} 文件读取债券余额数据/债券评级')
+            with open(json_file_path, 'r') as json_file:
+                data = json.load(json_file)
+
+            if consider_balance:
+                data_balances = data[0]["ths_bond_balance_cbond"]
+            if consider_issue:
+                data_issues = data[0]["ths_issue_credit_rating_cbond"]
         return data_balances, data_issues
 
     # 获取数据
     def get_data_basics(self, start_date, end_date):
         file_handler = FileHandler()
-        delta = datetime.timedelta(days=1)
         data_list = []
         print("基本数据获取")
 
@@ -111,13 +124,14 @@ class Ths:
 
             if os.path.exists(json_file_path):
                 # 指定JSON文件路径
-                print(f'从文件{json_file_path}中读取数据')
+                print(f'{current_date} 文件读取')
                 # 从JSON文件读取数据
                 with open(json_file_path, 'r') as json_file:
                     data = json.load(json_file)
 
                 data_list.append((current_date.strftime("%Y-%m-%d"), data))
             else:
+                print(f'{current_date} 接口获取')
                 get_str = 'edate=' + edate + ';zqlx=全部'
                 # jydm交易代码 f027转换价值 f022转股溢价率
                 data = THS_DR('p00868', get_str, 'jydm:Y,p00868_f027:Y,p00868_f022:Y', 'format:list')
@@ -146,7 +160,7 @@ class CPR:
         min_balance = 3
 
         # 债券评级
-        consider_issue = False
+        consider_issue = True
         issue = "AA+"
 
         float_values = []
@@ -199,8 +213,8 @@ def main():
     ths = Ths()
     file_handler = FileHandler()
     cpr = CPR()
-    username = ""
-    password = ""
+    username = "zyzqsx112"
+    password = "935b43"
     ths.login(username, password)
 
     start_date = datetime.date(2024, 1, 2)
