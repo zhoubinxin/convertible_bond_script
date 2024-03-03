647433b4556f5958e2f09ba62bf80f276e349928
zhoubinxin <binxin.zhou@outlook.com>
2024-03-03 22:00:42 +0800

    refactor:

--- bond/__init__.py
+++ bond/__init__.py
@@ -1,10 +1,20 @@
+from . import bonddb
+
+
 class Bond(object):
-    def __init__(self, name, cusip, face_value, coupon_rate, maturity):
-        self.name = name
-        self.cusip = cusip
-        self.face_value = face_value
-        self.coupon_rate = coupon_rate
-        self.maturity = maturity
+    def __init__(self):
+        # self.name = name
+        # self.cusip = cusip
+        # self.face_value = face_value
+        # self.coupon_rate = coupon_rate
+        # self.maturity = maturity
+        pass
 
     def __repr__(self):
-        return f"Bond({self.name}, {self.cusip}, {self.face_value}, {self.coupon_rate}, {self.maturity})"
+        return f"Bond({self.name})"
+
+    def query_data(self):
+        database_url = 'mysql+pymysql://root:123456@localhost/convertible_bond'
+        bond_db = bonddb.BondDB(database_url, '20180101')
+        data = bond_db.query_data('`代码`')
+        return data
--- bond/bonddb.py
+++ bond/bonddb.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine, Column, String, Float, BigInteger
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+Base = declarative_base()
 
 class BondDB(object):
     def __init__(self, db_url, table_name):
@@ -10,63 +11,19 @@ class BondDB(object):
         self.Session = sessionmaker(bind=self.engine)
         self.table_name = table_name
 
-    def create_table(self):
-        self.Base.metadata.create_all(self.engine)
-
-    def define_bond_class(self):
-        class ConvertibleBond(self.Base):
-            __tablename__ = self.table_name
-
-            # 列名中的特殊字符使用反引号括起来
-            code = Column(String, name='代码', primary_key=True)
-            name = Column(String, name='名称')
-            trade_date = Column(String, name='交易日期')
-            previous_close_price = Column(Float, name='前收盘价')
-            open_price = Column(Float, name='开盘价')
-            highest_price = Column(Float, name='最高价')
-            lowest_price = Column(Float, name='最低价')
-            close_price = Column(Float, name='收盘价')
-            rise_fall = Column(Float, name='涨跌')
-            rise_fall_range = Column(Float, name='涨跌幅(%)')
-            accrued_interest_days = Column(BigInteger, name='已计息天数')
-            accrued_interest = Column(Float, name='应计利息')
-            remain_term = Column(Float, name='剩余期限(年)')
-            current_yield = Column(Float, name='当期收益率(%)')
-            ytm = Column(Float, name='纯债到期收益率(%)')
-            pure_bond_value = Column(Float, name='纯债价值')
-            pure_bond_premium = Column(Float, name='纯债溢价')
-            pure_bond_premium_ratio = Column(Float, name='纯债溢价率(%)')
-            conversion_price = Column(Float, name='转股价格')
-            conversion_ratio = Column(Float, name='转股比例')
-            conversion_value = Column(Float, name='转换价值')
-            conversion_premium = Column(Float, name='转股溢价')
-            conversion_premium_rate = Column(Float, name='转股溢价率(%)')
-            conversion_p2e_ratio = Column(Float, name='转股市盈率')
-            conversion_p2b_ratio = Column(Float, name='转股市净率')
-            arbitrage_space = Column(Float, name='套利空间')
-            parity = Column(Float, name='平价/底价')
-            term = Column(BigInteger, name='期限(年)')
-            issue_date = Column(String, name='发行日期')
-            coupon_rate = Column(Float, name='票面利率/发行参考利率(%)')
-            trade_floor = Column(String, name='交易市场')
-            bond_type = Column(String, name="债券类型")
-
-        return ConvertibleBond
-
-    def add_bond(self, bond_data):
+    def query_data(self, column):
         Session = self.Session()
-        BondClass = self.define_bond_class()
-        bond = BondClass(**bond_data)
-        Session.add(bond)
-        Session.commit()
-        Session.close()
+        try:
+            # 获取表的元数据
+            table_class = Base.metadata.tables[self.table_name]
 
-    def get_all_bonds(self):
-        Session = self.Session()
-        BondClass = self.define_bond_class()
-        bonds = Session.query(BondClass).all()
-        Session.close()
-        return bonds
+            # 查询指定列的数据
+            result = Session.query(table_class.c[column]).all()
+            return result
+        except Exception as e:
+            print(f"查询出错: {str(e)}")
+        finally:
+            Session.close()
 
 
 # 使用示例
